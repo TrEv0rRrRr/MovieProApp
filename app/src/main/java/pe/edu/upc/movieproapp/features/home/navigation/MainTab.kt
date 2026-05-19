1ef52a30187836138b9a150a64ea14a2c0a3e656
TrEv0rRrRr <valentinosolischang@email.com>
2026-05-19 12:36:16 -0500

    Implement navigation routes and tab configuration
    - Create `MainTab` enum to define labels and icons for Home, Favorites, and Popular tabs.
    - Define `HomeRoute`, `FavoritesRoute`, and `PopularRoute` as serializable objects for type-safe navigation.
    - Add vector drawable resources for `home`, `favorite`, and `popular` icons.

---
+++ app/src/main/java/pe/edu/upc/movieproapp/features/home/navigation/MainTab.kt
@@ -0,0 +1,12 @@
+package pe.edu.upc.movieproapp.features.home.navigation
+
+import pe.edu.upc.movieproapp.R
+
+enum class MainTab(
+    val label: String,
+    val id: Int
+) {
+    Home("Home", id = R.drawable.home),
+    Favorites("Favorites", id = R.drawable.favorite),
+    Popular("Popular", id = R.drawable.popular),
+}
---
+++ app/src/main/java/pe/edu/upc/movieproapp/features/home/navigation/Routes.kt
@@ -0,0 +1,12 @@
+package pe.edu.upc.movieproapp.features.home.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object HomeRoute
+
+@Serializable
+object FavoritesRoute
+
+@Serializable
+object PopularRoute
